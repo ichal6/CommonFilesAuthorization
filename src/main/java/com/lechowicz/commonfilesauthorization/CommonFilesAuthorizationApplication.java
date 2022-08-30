5fc7d51ea4f938b31dcb6cf22307fe8106d1b96c
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-08-30 19:48:41 +0200

    Implement basic version

---
+++ src/main/java/com/lechowicz/commonfilesauthorization/CommonFilesAuthorizationApplication.java
@@ -0,0 +1,14 @@
+package com.lechowicz.commonfilesauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CommonFilesAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonFilesAuthorizationApplication.class, args);
+	}
+
+}
